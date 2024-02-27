34ec90e6bceb4e50a66687241237511e46595625
FMir55 <s1006005@gmail.com>
2024-02-27 07:46:05 +0000

    baseline

---
+++ mysql.py
@@ -0,0 +1,10 @@
+#%%
+import mysql.connector
+
+# %%
+connection = mysql.connector.connection(host='localhost', port='3306')
+# %%
+import numpy as np
+# %%
+!pip install mysql-connector-python
+# %%
